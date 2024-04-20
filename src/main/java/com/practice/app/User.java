1ea768b76f9c4baee51a2c4735b9e03b39b890fe
arashmpr <arashmdp@gmail.com>
2024-04-20 19:19:36 +0330

    initial commit

---
+++ src/main/java/com/practice/app/User.java
@@ -0,0 +1,22 @@
+package com.practice.app;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Long id;
+    private String role;
+    private String username;
+    private String password;
+    private String email;
+    private String address;
+}
+
+
