d7b5dc36184ddaaa17f6a7da1be7cf29692f5db6
arashmpr <arashmdp@gmail.com>
2024-04-21 03:33:38 +0330

    feat: validateUser added to UserManagerment

--- src/main/java/com/practice/app/TableReservation.java
+++ src/main/java/com/practice/app/TableReservation.java
@@ -4,7 +4,7 @@ public class TableReservation {
     private UserManager userManager = new UserManager();
 
     public void run() {
-        this.userManager.addUser("client", "arash", "set12", "email", "address");
+        this.userManager.addUser("manager", "ara2@sh", "set12", "email", "address");
         System.out.println("The user is added to the thing!");
     }
 }
--- src/main/java/com/practice/app/UserManager.java
+++ src/main/java/com/practice/app/UserManager.java
@@ -1,5 +1,6 @@
 package com.practice.app;
 
+import javax.xml.validation.Validator;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,7 +9,39 @@ public class UserManager {
 
     public void addUser(String role, String username, String password, String email, String address) {
         User newUser = new User(role, username, password, email, address);
-        users.add(newUser);
-        System.out.println("The number of users are: " + users.size());
+        boolean userValidation = validateUser(newUser);
+        if (userValidation) {
+            users.add(newUser);
+            System.out.println(users.size() + " user have joined TableReservation application :)");
+        }
+        else {
+            System.out.println(users.size() + " Sorry user is not valid :(");
+
+        }
+
     }
+
+    private boolean validateUser(User user) {
+        boolean roleValidation = validateUserRole(user.getRole());
+        boolean usernameValidation = validateUserUsername(user.getUsername());
+        if (roleValidation && usernameValidation) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean validateUserRole(String role) {
+        if (role.equals("client") || role.equals("manager")) {
+            return true;
+        }
+        System.out.println("User role has some fucked up stuff!");
+        return false;
+    }
+
+    private boolean validateUserUsername(String username) {
+        String regex = "^[^!@#$%^&*()+=\\[\\]{};:'\"\\\\|,.<>/?~`]+$";
+        return username.matches(regex);
+    }
+
+
 }
